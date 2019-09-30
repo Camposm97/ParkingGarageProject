4f35295d8808c539f81c56c520d06bf9a8a0be1b
Camposm97 <camposm1205@gmail.com>
2019-09-30 00:05:08 -0400

    Tweaked Menus for GarageMenuBar

--- src/util/MenuUtil.java
+++ src/util/MenuUtil.java
@@ -1,19 +1,39 @@
 package util;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import javafx.application.Platform;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 public class MenuUtil {
 	public static List<Menu> loadMenus() {
-		List<Menu> list = new ArrayList<>();
-		Menu m1 = new Menu("File");
-		Menu m2 = new Menu("Edit");
-		Menu m3 = new Menu("Help");
-		list.add(m1);
-		list.add(m2);
-		list.add(m3);
+		List<Menu> list = new LinkedList<>();
+		list.add(loadMenuFile());
+		list.add(loadMenuEdit());
+		list.add(loadMenuHelp());
 		return list;
+	}
+	
+	public static Menu loadMenuFile() {
+		MenuItem mi = new MenuItem("Exit");
+		mi.setOnAction(e -> {
+			// Before exiting, save data (not yet implemented)
+			Platform.exit();
+		});
+		Menu menu = new Menu("File");
+		menu.getItems().add(mi);
+		return menu;
+	}
+	
+	public static Menu loadMenuEdit() {
+		Menu menu = new Menu("Edit");
+		return menu;
+	}
+	
+	public static Menu loadMenuHelp() {
+		Menu menu = new Menu("Help");
+		return menu;
 	}
 }	
