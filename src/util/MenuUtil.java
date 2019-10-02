fc85ba424666bef3829035bcc36759543d0207c9
Camposm97 <camposm1205@gmail.com>
2019-10-02 07:32:38 -0400

    Modified MenuUtil.java (WIP)

--- src/util/MenuUtil.java
+++ src/util/MenuUtil.java
@@ -1,6 +1,7 @@
 package util;
 
 import static util.Web.*;
+import static util.ImgUtil.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,6 +21,7 @@ public class MenuUtil {
 	
 	public static Menu loadMenuFile() {
 		MenuItem mi = new MenuItem("Exit");
+		mi.setGraphic(loadImgV(EXIT_ICON));
 		mi.setOnAction(e -> {
 			// Before exiting, save data (not yet implemented)
 			Platform.exit();
@@ -30,7 +32,14 @@ public class MenuUtil {
 	}
 	
 	public static Menu loadMenuEdit() {
+		MenuItem miUser = new MenuItem("User");
+		MenuItem miCar = new MenuItem("Car");
+		Menu mInsert = new Menu("Insert");
+		mInsert.getItems().addAll(miUser, miCar);
+		Menu mDelete = new Menu("Delete");
+		mDelete.getItems().addAll(miUser, miCar);
 		Menu m = new Menu("Edit");
+		m.getItems().addAll(mInsert, mDelete);
 		return m;
 	}
 	
@@ -42,6 +51,7 @@ public class MenuUtil {
 		MenuItem mi3 = new MenuItem("Chris Demonte");
 		mi3.setOnAction(e -> { browse(DEMONTE_GITHUB); });
 		Menu m2 = new Menu("Developer's Github");
+		m2.setGraphic(loadImgV(GITHUB_ICON));
 		m2.getItems().addAll(mi1, mi2, mi3);
 		Menu m1 = new Menu("Help");
 		m1.getItems().add(m2);
