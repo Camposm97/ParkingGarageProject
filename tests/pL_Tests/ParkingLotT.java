b903a42b2db88abcb2515c886ff729d4bac2c9a7
sayhimatt <mattsayshi@hotmail.com>
2019-10-04 19:57:47 -0400

    GUI sketch

--- tests/pL_Tests/ParkingLotT.java
+++ tests/pL_Tests/ParkingLotT.java
@@ -15,5 +15,14 @@ class ParkingLotT {
 		ParkingLot t = new ParkingLot(1, 0, 0);
 		assertNotNull(t.spaceFinder(VehicleType.CAR, false));
 	}
+	@Test
+	void overRideTest() {
+		ParkingLot t = new ParkingLot(0, 0, 1);
+		// Ask it to find a spot for a car if we can override and place it in a truck spot
+		assertNotNull(t.spaceFinder(VehicleType.CAR, true));
+		// If it cannot be found then we return -1
+		assertEquals(t.spaceFinder(VehicleType.CAR, false), -1);
+	}
+	
 
 }
