4449007b3bb035c26ba52b5e2013223aaa114825
Camposm97 <camposm1205@gmail.com>
2019-10-04 18:45:58 -0400

    When user logins in, the stage is resized to a bigger window

--- src/control/LoginButton.java
+++ src/control/LoginButton.java
@@ -1,8 +1,11 @@
 package control;
 
+import app.App;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import userData.UserData;
 import userData.UserDataManager;
 import view.LoginPane;
 import view.MainMenu;
@@ -22,8 +25,14 @@ public class LoginButton extends Button {
 			UserDataManager users = login.getUsers();
 			String username = login.getUsername();
 			String password = login.getPassword();
-			if (users.passwordCheck(username, password))
+			UserData user = users.login(username, password);
+			if (user != null) {
+				Stage stage = (Stage) login.getScene().getWindow();
+				stage.setWidth(App.WIDTH);
+				stage.setHeight(App.HEIGHT);
+				stage.centerOnScreen();
 				login.getScene().setRoot(new MainMenu(users, null));
+			}
 		}
 	}
 }
