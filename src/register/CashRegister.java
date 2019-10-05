d5fcd0976027de4e4f64729ef1f7ff43667e321f
sayhimatt <mattsayshi@hotmail.com>
2019-10-04 23:46:14 -0400

    GP update

--- src/register/CashRegister.java
+++ src/register/CashRegister.java
@@ -76,7 +76,7 @@ public class CashRegister {
 
 	public void loadData() {
 		LocalDate date = LocalDate.now();
-		String adr = "/resources/daily" + date.getMonthValue() + date.getDayOfMonth() + ".data";
+		String adr = "/resources/daily" + date.getMonthValue() + date.getDayOfMonth() ;
 		File file = new File(adr);
 		boolean exists = file.exists();
 		if (exists) {
