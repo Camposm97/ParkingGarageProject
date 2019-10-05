1cd2d2bce4ef87590626adc30b3b85f5c11c65d6
Camposm97 <camposm1205@gmail.com>
2019-10-04 23:20:58 -0400

    Implemented DeleteUserWindow

--- src/view/DeleteUserWindow.java
+++ src/view/DeleteUserWindow.java
@@ -3,20 +3,32 @@ package view;
 import java.util.Optional;
 
 import app.App;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextInputDialog;
+import userData.UserData;
 import userData.UserDataManager;
 
 public class DeleteUserWindow extends TextInputDialog {
-	private UserDataManager users;
 	
 	public DeleteUserWindow(UserDataManager users) {
-		this.users = users;
 		super.setTitle(App.TITLE);
 		super.setHeaderText("Please enter the name of the user you'd like to delete:");
 		super.setContentText("Username:");
 		Optional<String> result = this.showAndWait();
 		if (result.isPresent()) {
-			System.out.println(result.get());
+			int index = users.getIndexFromUserName(result.get());
+			UserData user = users.getUserList().get(index);
+			user.closeAccount();
+			showDisabledAccount(user);
 		}
+	}
+	
+	private void showDisabledAccount(UserData user) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(App.TITLE);
+		alert.setHeaderText("Successfully removed: " + user.getUserName());
+		alert.setContentText("This account is now closed.");
+		alert.showAndWait();
 	}
 }
