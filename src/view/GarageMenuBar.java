fef98fec0e5c56f6be6e92314cfe5ce8f077c664
chrisdemonte <chris.demonte@yahoo.com>
2019-10-05 17:52:28 -0400

    Hopefully no errors

--- src/view/GarageMenuBar.java
+++ src/view/GarageMenuBar.java
@@ -90,8 +90,8 @@ public class GarageMenuBar extends MenuBar {
 		MenuItem miAddCar = new MenuItem("Car (Check-In)");
 		miAddCar.setOnAction(e -> {
 			BorderPane root = (BorderPane) super.getParent();
-			GarageTableView garageTable = new GarageTableView(spaces, new BorderPane());
 			BorderPane borderPane = new BorderPane(new CheckInPane(spaces));
+			GarageTableView garageTable = new GarageTableView(spaces, borderPane);
 			borderPane.setRight(garageTable.getContainer());
 			root.setCenter(borderPane);
 		});
