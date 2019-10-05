e4a5e2452ff489ab8d5a9a7d7207052965b67e99
sayhimatt <mattsayshi@hotmail.com>
2019-10-04 22:37:45 -0400

    Added isDisabled
    UserData class has a boolean called isDisabled to disable the account. The account named "admin" with ID 0 cannot be disabled.

--- tests/register_Tests/TicketT.java
+++ tests/register_Tests/TicketT.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TicketT {
-
+	
+	
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTicketCreation() {
+		
 	}
 
 }
