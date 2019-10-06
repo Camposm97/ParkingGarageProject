e7d581922e3a6e4155d765d489c2cec8e40829a6
Camposm97 <camposm1205@gmail.com>
2019-10-06 00:01:56 -0400

    Successfully created columns to display UserData info

--- src/view/UserDataViewer.java
+++ src/view/UserDataViewer.java
@@ -18,15 +18,21 @@ public class UserDataViewer extends BorderPane {
 	public TableView<UserData> loadTvUsers() {
 		TableView<UserData> tv = new TableView<>();
 		TableColumn<UserData, String> colUsername = new TableColumn<>("Username");
-		colUsername.setCellValueFactory(new PropertyValueFactory<UserData, String>("Username"));
+		colUsername.setCellValueFactory(new PropertyValueFactory<UserData, String>("userName"));
 		TableColumn<UserData, String> colPassword = new TableColumn<>("Password");
-		colPassword.setCellValueFactory(new PropertyValueFactory<UserData, String>("Password"));
+		colPassword.setCellValueFactory(new PropertyValueFactory<UserData, String>("password"));
+		TableColumn<UserData, String> colFirstName = new TableColumn<>("First Name");
+		colFirstName.setCellValueFactory(new PropertyValueFactory<UserData, String>("firstName"));
+		TableColumn<UserData, String> colLastName = new TableColumn<>("Last Name");
+		colLastName.setCellValueFactory(new PropertyValueFactory<UserData, String>("lastName"));
 		TableColumn<UserData, Boolean> colDisabled = new TableColumn<>("Disabled");
 		colDisabled.setCellValueFactory(new PropertyValueFactory<UserData, Boolean>("Disabled"));
 		TableColumn<UserData, Boolean> colAdmin = new TableColumn<>("Admin");
 		colAdmin.setCellValueFactory(new PropertyValueFactory<UserData, Boolean>("Admin"));
 		tv.getColumns().add(colUsername);
 		tv.getColumns().add(colPassword);
+		tv.getColumns().add(colFirstName);
+		tv.getColumns().add(colLastName);
 		tv.getColumns().add(colDisabled);
 		tv.getColumns().add(colAdmin);
 		tv.getItems().addAll(users.getUserList());
