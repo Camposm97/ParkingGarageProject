be3db9427cd1e516686d742bda27d130e4450f34
Camposm97 <camposm1205@gmail.com>
2019-10-05 23:47:53 -0400

    Still working on UserDataViewer.java

--- src/view/UserDataViewer.java
+++ src/view/UserDataViewer.java
@@ -1,16 +1,35 @@
 package view;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import model.UserData;
 import model.UserDataManager;
 
 public class UserDataViewer extends BorderPane {
 	private UserDataManager users;
-	private TableView<UserData> tvUsers;
-	
+
 	public UserDataViewer(UserDataManager users) {
 		this.users = users;
-		tvUsers = new TableView<UserData>();
+		super.setCenter(loadTvUsers());
+	}
+
+	public TableView<UserData> loadTvUsers() {
+		TableView<UserData> tv = new TableView<>();
+		TableColumn<UserData, String> colUsername = new TableColumn<>("Username");
+		colUsername.setCellValueFactory(new PropertyValueFactory<UserData, String>("Username"));
+		TableColumn<UserData, String> colPassword = new TableColumn<>("Password");
+		colPassword.setCellValueFactory(new PropertyValueFactory<UserData, String>("Password"));
+		TableColumn<UserData, Boolean> colDisabled = new TableColumn<>("Disabled");
+		colDisabled.setCellValueFactory(new PropertyValueFactory<UserData, Boolean>("Disabled"));
+		TableColumn<UserData, Boolean> colAdmin = new TableColumn<>("Admin");
+		colAdmin.setCellValueFactory(new PropertyValueFactory<UserData, Boolean>("Admin"));
+		tv.getColumns().add(colUsername);
+		tv.getColumns().add(colPassword);
+		tv.getColumns().add(colDisabled);
+		tv.getColumns().add(colAdmin);
+		tv.getItems().addAll(users.getUserList());
+		return tv;
 	}
 }
