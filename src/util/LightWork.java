ce2be3a4b4f2559654188f2db9d9af0d2449259b
Camposm97 <camposm1205@gmail.com>
2019-10-06 15:04:45 -0400

    Added Documentation to LightWork.java

--- src/util/LightWork.java
+++ src/util/LightWork.java
@@ -11,14 +11,28 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+ * Purpose of this class is to provide methods that will be used often throughout the program.  
+ * @author Michael Campos
+ */
 public class LightWork { // Originally named CamposFX
 	public static final String GARAGE_SRC = "systemData/garage.dat";
 	public static final Insets DEFAULT_INSETS = new Insets(10);
 	
+	/**
+	 * Takes the given date and returns a path utilizing it.  
+	 * @param date
+	 * @return String
+	 */
 	public static String emitDailyLog(LocalDate date) {
 		return "systemData/log/daily_" + date.getYear() + "_" + date.getMonthValue() + "_" + date.getDayOfMonth() + ".log";
 	}
 	
+	/**
+	 * Returns a VBox filled with nodes using the given parameter(s).
+	 * @param nodes
+	 * @return VBox
+	 */
 	public static VBox loadVBox(Node...nodes) {
 		VBox vBox = new VBox(10);
 		for (Node n : nodes)
@@ -26,6 +40,11 @@ public class LightWork { // Originally named CamposFX
 		return vBox;
 	}
 	
+	/**
+	 * Creates a HBox filled with nodes using the given parameter(s).
+	 * @param nodes
+	 * @return HBox
+	 */
 	public static HBox loadHBox(Node...nodes) {
 		HBox hBox = new HBox(10);
 		for (Node n : nodes)
@@ -33,6 +52,10 @@ public class LightWork { // Originally named CamposFX
 		return hBox;
 	}
 	
+	/**
+	 * 
+	 * @param gridPane
+	 */
 	public static void initGridPaneSettings(GridPane gridPane) {
 		gridPane.setHgap(10);
 		gridPane.setVgap(10);
@@ -40,6 +63,12 @@ public class LightWork { // Originally named CamposFX
 		gridPane.setAlignment(Pos.CENTER);
 	}
 	
+	/**
+	 * Returns a Generic ComboBox filled with values by using the given array.
+	 * @param <T>
+	 * @param arr
+	 * @return ComboBox<T>
+	 */
 	public static <T> ComboBox<T> loadCb(T[] arr) {
 		ComboBox<T> cb = new ComboBox<>();
 		for (T elem : arr)
@@ -47,6 +76,12 @@ public class LightWork { // Originally named CamposFX
 		return cb;
 	}
 	
+	/**
+	 * Returns of a Generic ComboBox filled with values by using the given List.
+	 * @param <T>
+	 * @param list
+	 * @return ComboBox<T>
+	 */
 	public static <T> ComboBox<T> loadCb(List<T> list) {
 		ComboBox<T> cb = new ComboBox<>();
 		for (T elem : list)
